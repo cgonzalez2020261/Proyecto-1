36a5a06b50674d1772d0f7f54406a552b91a5012
cgonzalez2020261 <89095623+cgonzalez2020261@users.noreply.github.com>
2022-04-20 10:13:45 -0600

    proyecto terminado
    culminacion de proyecto 1

---
+++ static/scripts/main.js
@@ -0,0 +1,22 @@
+const contador = document.getElementById("contar");
+const sumar = document.getElementById("incr");
+const restar = document.getElementById("decr");
+const reset = document.getElementById("reset");
+
+let numero = 0;
+
+sumar.addEventListener("click", ()=>{
+    numero++;
+    contador.innerHTML = numero;
+});
+
+restar.addEventListener("click", ()=>{
+
+    numero -= 1;
+    contador.innerHTML = numero;
+});
+
+reset.addEventListener("click", ()=>{
+    numero = 0;
+    contador.innerHTML = numero;
+})
